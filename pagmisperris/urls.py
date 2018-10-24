dcc7a51a06c174c166bba8124f5b7c41f024d321
patricio huentemil <p_huente@gmail.com>
2018-10-24 18:21:25 -0300

    Cambie el HTML para la página.

--- pagmisperris/urls.py
+++ pagmisperris/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='base'),
+    #path('', views.base, name='base'),
+	path('', views.post_list, name='post_list'),
 ]
--- pagmisperris/views.py
+++ pagmisperris/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def base(request):
-    return render(request, 'pagmisperris/base.html', {})
+#def base(request):
+    #return render(request, 'pagmisperris/base.html', {})
+
+def post_list(request):
+    return render(request, 'pagmisperris/post_list.html', {})
